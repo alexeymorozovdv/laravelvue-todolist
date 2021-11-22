68803a1882332056bd61e441a293a8c9a21d54ed
alexeymorozovdv <alexeymorozov.dev@gmail.com>
2021-11-22 12:12:55 +0300

    Added output all items on main page, added possibility to check item as 'done' and a button to delete item

--- resources/js/components/ListItem.vue
+++ resources/js/components/ListItem.vue
@@ -1,15 +1,51 @@
 <template>
-    <div>
-        List Item
+    <div class="item">
+        <input
+            type="checkbox"
+            @change="updateCheck()"
+            v-model="item.completed"
+        />
+        <span :class="[item.completed ? 'completed' : '', 'item-text']">
+            {{item.name}}
+        </span>
+
+        <button @click="removeItem()" class="trashCan">
+            <font-awesome-icon icon="trash"/>
+        </button>
+
     </div>
 </template>
 
 <script>
 export default {
-    name: "ListItem"
+    props: ['item']
 }
 </script>
 
 <style scoped>
+
+.completed {
+    text-decoration: line-through;
+    color: #999999;
+}
+
+.item-text {
+    width: 100%;
+    margin-left: 20px;
+}
+
+.item {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.trashCan {
+    background: #e6e6e6;
+    border: none;
+    color: #ff0000;
+    outline: none;
+    cursor: pointer;
+}
 
 </style>
